d1db2bc40c32efc9e922d70790a11b9a66df3c56
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-25 00:21:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+my_list = list(range(-100, 101))
+
+
+middle_three= my_list[99:102]
+
+print(middle_three)
+
+
+# print(my_list[0])
+# print(my_list[-1])
+
+# print(middle)
+# middle_three = list(((sum(my_list))/2)-1, (sum(my_list)), /2,((sum(my_list))/2)+1) 
+
+# midpoint= int(((my_list[0])+(my_list[-1]))/2)
+
+# midpoint_1 = int((((my_list[0])+(my_list[-1]))/2)+1)
+
+# midpoint__1 = int((((my_list[0])+(my_list[-1]))/2)-1)
